1750f8024a347b542beec9b4270e244139b2be86
lucasazuma <azumalucas@gmail.com>
2021-08-22 10:40:59 -0300

    first draft of add function

--- simulador/instructions.c
+++ simulador/instructions.c
@@ -7,12 +7,12 @@
 
 
 void add(uint32_t rd_index, uint32_t rs1_index, uint32_t rs2_index){
-    uint32_t rs1 = lb(rs1_index, 0);
-    uint32_t rs2 = lb(rs2_index, 0);
+    uint32_t rs1 = lw(rs1_index, 0);
+    uint32_t rs2 = lw(rs2_index, 0);
     int decimal_rs1 = hex_to_decimal(rs1);
     int decimal_rs2 = hex_to_decimal(rs2);
     int decimal_rd = decimal_rs1 + decimal_rs2;
-    sb(rd_index, 0, decimal_to_hex(decimal_rd));
+    sw(rd_index, 0, decimal_to_hex(decimal_rd));
 }
 
 
